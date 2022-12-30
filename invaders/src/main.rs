b3a1cfa92652830023444c5a07137779f0cb874a
Federico <f.fulgeri@gmail.com>
2022-12-30 16:33:23 +0100

    working windows

--- invaders/src/main.rs
+++ invaders/src/main.rs
@@ -1,11 +1,17 @@
 use std::error::Error;
 use rusty_audio::Audio;
-use std::io;
+use std:: {io, thread};
+use std::sync::mpsc::{self, Receiver};
 use crossterm::{terminal, ExecutableCommand};
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use crossterm::cursor::{Hide, Show};
 use crossterm::{event::{self, Event, KeyCode}};
-use std::time::Duration;
+use std::time::{Duration, Instant};
+
+use invaders::{
+    frame::{self, new_frame, Drawable, Frame},
+    render
+};
 
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -25,8 +31,29 @@ fn main() -> Result<(), Box<dyn Error>> {
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
 
-    //main loop
+
+    // Render loop in a separate thread
+
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+
+            render::render(&mut stdout, &last_frame, &curr_frame, true);
+            last_frame = curr_frame;
+        }
+    });
+
+    // Game loop
     'gameloop: loop {
+        // Per frame init 
+        let curr_frame = new_frame();
         //input event
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()?{
@@ -39,9 +66,15 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
             }
         }
+
+        //Draw & render
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
     }
 
     //clean up
+    drop(render_tx);
+    render_handle.join().unwrap();
     audio.wait();
     stdout.execute(Show)?;
     stdout.execute(LeaveAlternateScreen)?;
